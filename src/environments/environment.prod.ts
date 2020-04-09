948af74c563235876b7d67ba70a70d9afdcad13c
Chris Patton <cd.patton@gmail.com>
2020-04-09 15:23:17 -0700

    Added Firebase Analytics

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBOQ_3O8sH0KYC6woZ17_lZ2MBZxpJ8P6Q",
+    authDomain: "covidcalc.firebaseapp.com",
+    databaseURL: "https://covidcalc.firebaseio.com",
+    projectId: "covidcalc",
+    storageBucket: "covidcalc.appspot.com",
+    messagingSenderId: "301822685794",
+    appId: "1:301822685794:web:a45bdba131c3526b24173b",
+    measurementId: "G-D1D3M748P6"
+  }
 };
