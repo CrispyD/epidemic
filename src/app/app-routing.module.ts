c51eb0be55cc8c81573f3de8e13081d98389bb17
Chris Patton <cd.patton@gmail.com>
2020-04-14 15:27:44 -0700

    added about

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { AboutComponent } from './about/about.component';
 
 
 const routes: Routes = [
   { path: '', component: DashboardComponent },
+  { path: 'about', component: AboutComponent },
 ];
 
 @NgModule({
